be212b1542eea1d732fc968e8fd85bbd76bc2631
t3min4l <hainam.nguyen.ict@gmail.com>
2018-09-25 00:01:11 +0700

    upate

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ import torchvision.transforms as transforms
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-
+from skimage.feature import greycomatrix
 
 SUBDATASET_DIR = os.path.join(constants.DATA_DIR, 'subdataset')
 TEST_IMAGE = '/home/t3min4l/workspace/texture-classification/data/subdataset/canvas1/canvas1-a-p001.png'
@@ -22,121 +22,16 @@ TEST_IMAGE = '/home/t3min4l/workspace/texture-classification/data/subdataset/can
 
 folders = [x[0] for x in os.walk(SUBDATASET_DIR)]
 
-# print(folders)
-del folders[0]
-
-def get_labels(folders):
-    substr1 = 'subdataset/'
-    interval = len(substr1)
-    labels = list()
-    for folder in folders:
-        idx = folder.find(substr1)
-        label = folder[idx+interval:len(folder)]
-        labels.append(label)
-    return labels
-
-labels = get_labels(folders)
-# print(labels)
-
-def build_images_path_label(labels):
-    dataset = list()
-    for idx, label in enumerate(labels):
-        label_dir = os.path.join(SUBDATASET_DIR, label)
-        fns = glob.glob(label_dir+'/*.png')
-        print(label)
-        print(len(fns))
-        for fn in fns:
-            dataset.append([fn, idx])
-    return dataset
-
-def extract_features(image):
-    textures = mt.features.haralick(image)
-    print(textures)
-    ht_mean = textures.mean(axis=0)
-
-    return ht_mean
-
-dataset = build_images_path_label(labels)
-shuffle(dataset)
-# print(dataset)
-
-class AlexNet(nn.Module):
-
-    def __init__(self, num_classes=1000):
-        super(AlexNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 7 * 7, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        print(x.shape)
-        x = x.view(x.size(0), 256 * 7 * 7)
-        x = self.classifier(x)
-        return x
-
-
-image = Image.open(TEST_IMAGE)
-tmp = image.getpixel((0,0))
-if isinstance(tmp, int) or len(tmp) != 3:
-    image = image.convert('RGB')
-
-image = cv2.imread(TEST_IMAGE)
-image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_CUBIC)
-cv2.imshow('origin image',image)
-cv2.waitKey(5000)
-cv2.destroyAllWindows()
-image_transformer = transforms.Compose([
-        # transforms.CenterCrop(256),
-        transforms.ToTensor(),
-    ])
-
-# image_input = image_transformer(image)
-# image_input = image_input.unsqueeze(0)
-# print(image_input.shape)
-# image_input = image_input.permute(0,3,1,2)
-# print(image_input.shape)
-# output = model(image_input)
-# print(output)
-# print(list(model.features.children()))
-from skimage import io 
-from sklearn import preprocessing
-from skimage import feature
 image = cv2.imread(TEST_IMAGE)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-graycomatrix1 = feature.greycomatrix(image, distances=[1, 2], angles=[0, np.pi/2], levels=256, symmetric=True, normed=True)
-graycomatrix = graycomatrix1[0:256, 0:256, 0, 0]
-# cv2.imshow('graycomatrix1', graycomatrix1)
-print(graycomatrix1)
-# fig = plt.figure(figsize=(4,4))
 
-# ax = fig.add_subplot(3,3,1)
-# ax.imshow(graycomatrix1,cmap=plt.cm.gray, interpolation='nearest', vmin=0, vmax=255)
-# ax.set_xlabel('Gray co-matrix')
-# plt.show()
-contrast = feature.greycoprops(graycomatrix1, prop='contrast')
-dissimilarity = feature.greycoprops(graycomatrix1, prop='dissimilarity')
-print('contrast:{}.dissimilarity:{}'.format(contrast, dissimilarity))
-print(contrast.shape)
-print(dissimilarity.shape)
+print(type(image))
+
+contrast_glcm = np.zeros(image.shape)
+dissimilarity_glcm = np.zeros(image.shape)
+energy_glcm = np.zeros(image.shape)
+correlation_glcm = np.zeros(image.shape)
+ASM_glcm = np.zeros(image.shape)
+
+for i in range(image.shape[0]):
+    for j in range(image.shape[1])
